751d40593f2f7e33001714b20e324e13b139b4d3
shoichi nagasaki <clark.zoro@gmail.com>
2023-10-16 20:49:12 +0900

    jQuery append sample port script

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ $(function () {
   $("p").css("color", "#f00");
   $("p").css({ color: "#f00", "font-weight": "bold" });
   $("p").append("サンプル")
+  $("p").css({"color":"f000","font-weight":"bold"}).append("sample");
 });
