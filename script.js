3adda65182cba3b058eed65317954bb722eacabf
shoichi nagasaki <clark.zoro@gmail.com>
2023-10-19 21:13:50 +0900

    jQuery function using ver. not using ver.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// 関数を使わない書き方
+// $(function(){
+//   $(".a").on("mouseover",function(){
+//     $(this).css("margin-left" ,"100px");
+//   });
+//   $(".b").on("mouseover",function(){
+//     $(this).css("margin-left","100px");
+//   });
+//   $(".c").on("mouseover",function(){
+//     $(this).css("margin-left","100px");
+//   });
+// });
+
+// 関数を使用した書き方
 $(function(){
-  $(".open").on("click",function() {
-    $(this).toggleClass("close");
-    $("dd").slideToggle();
-  });
+  $(".a").on("mouseover",sample);
+  $(".b").on("mouseover",sample);
+  $(".c").on("mouseover",sample);
+  function sample () {
+    $(this).css("margin-left","100px")
+  }
 });
