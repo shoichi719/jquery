c3b4b6e68154212cef1ff6799e17d35dd504ac2c
shoichi nagasaki <clark.zoro@gmail.com>
2023-10-19 21:23:13 +0900

    jQuery argument port script

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-// 関数を使わない書き方
-// $(function(){
-//   $(".a").on("mouseover",function(){
-//     $(this).css("margin-left" ,"100px");
-//   });
-//   $(".b").on("mouseover",function(){
-//     $(this).css("margin-left","100px");
-//   });
-//   $(".c").on("mouseover",function(){
-//     $(this).css("margin-left","100px");
-//   });
-// });
 
-// 関数を使用した書き方
 $(function(){
-  $(".a").on("mouseover",sample);
-  $(".b").on("mouseover",sample);
-  $(".c").on("mouseover",sample);
-  function sample () {
-    $(this).css("margin-left","100px")
-  }
+  // 引数を渡す
+  $(sample("佐藤"));
+  $(sample("山田"));
+  $(sample("鈴木"));
+  function sample (name)
+{
+  console.log("あなたの名前は" + name + "さんですね。");
+}
 });
