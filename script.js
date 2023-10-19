ad6f43faedafa4be984562653a3c21b4c0e89971
shoichi nagasaki <clark.zoro@gmail.com>
2023-10-19 21:45:19 +0900

    jQuery function can use calculate port script

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
 $(function(){
-  // 引数を渡す
-  $(sample("佐藤"));
-  $(sample("山田"));
-  $(sample("鈴木"));
-  function sample (name)
-{
-  console.log("あなたの名前は" + name + "さんですね。");
-}
-});
+  // 関数は計算ができる
+  addsample(1,2);
+  addsample(4,3);
+  addsample(100,1);
+
+    function addsample (num1,num2){
+      var rslt = num1 + num2;     
+      console.log(rslt);
+      // 条件分岐も使える
+      if(rslt == 3){
+        console.log("処理結果は"+rslt+"です");
+      }
+      else{
+        console.log("処理結果は３以外です");
+      }
+    }
+  });
